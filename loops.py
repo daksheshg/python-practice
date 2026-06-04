a7c19f000fa7142f48f743c61485f3a309d2f93d
daksheshgandhepfw <gandd01@pfw.edu>
2026-06-04 16:16:39 -0400

    Add loop practice exercises

---
+++ loops.py
@@ -0,0 +1,14 @@
+for num in range(1,11,1):
+    print(num)
+
+for num in range(2,22,2):
+    print(num)
+
+sum=0
+for i in range(1,101):
+    sum+=i;
+print(sum)
+
+for i in range(5,51,5):
+    print(i)
+
